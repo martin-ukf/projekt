4b0fcbecaaa7a991be452a229c9ebe7981c46d9b
martin-ukf <martin-ukf@users.noreply.github.com>
2017-04-20 11:09:40 +0200

    upravene menu

--- application/views/template/navigation.php
+++ application/views/template/navigation.php
@@ -3,35 +3,35 @@
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
+                <span class="sr-only">Projekt PDA</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a class="navbar-brand" href="#">Projekt PDA</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li class="active"><a href="#">Domov</a></li>
+                <li><a href="#about">Popis</a></li>
+                <li><a href="#contact">Zadanie</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Príklady <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
+                        <li><a href="<?php echo base_url('/index.php/users/')?>">DataTables</a></li>
+                        <li><a href="#">GroceryCRUD</a></li>
+                        <li><a href="#">Charts</a></li>
                         <li role="separator" class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
+                        <li class="dropdown-header">TBD</li>
+                        <li><a href="#">TBD</a></li>
+                        <li><a href="#">TBD</a></li>
                     </ul>
                 </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="../navbar/">Default</a></li>
-                <li><a href="../navbar-static-top/">Static top</a></li>
-                <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+                <li><a href="../navbar/">link</a></li>
+                <li><a href="../navbar-static-top/">link</a></li>
+                <li class="active"><a href="./"><span class="sr-only">(current)</span>link</a></li>
             </ul>
         </div><!--/.nav-collapse -->
     </div>
